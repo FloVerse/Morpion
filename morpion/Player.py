e824aa7c4e9d73162cd7118846c45b738a003a5a
f2bendo <92117591+f2bendo@users.noreply.github.com>
2022-11-06 23:57:16 +0100

    Add files via upload

---
+++ morpion/Player.py
@@ -0,0 +1,7 @@
+class Player :
+
+    def __init__(self,leCarac) -> None:
+        self.carac = leCarac
+    
+    def getCarac(self) :
+        return self.carac
---
+++ morpion/game.py
@@ -0,0 +1,16 @@
+from morpion import Morpion
+game = Morpion()
+game.create_board()
+
+""""dz
+i =2
+for row in game.board :
+    row[i] = 'X'
+    i-=1
+"""
+#game.display()
+#game.entry(0,0,'X')
+#game.display()
+game.start()
+
+#test gain game.entry(0,1,'X') game.entry(1,1,'X') game.entry(2,1,'X')
